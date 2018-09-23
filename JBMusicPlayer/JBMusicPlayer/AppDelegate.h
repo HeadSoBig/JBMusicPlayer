1e6b685262cee736dbaa7a9b5052105279b17eb8
cnliveJunBo <wangjunbo@cnlive.com>
2018-09-23 16:30:55 +0800

    Create project

---
+++ JBMusicPlayer/JBMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JBMusicPlayer
+//
+//  Created by cnliveJunBo on 2018/9/23.
+//  Copyright © 2018年 cnliveJunBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JBMusicPlayer/JBMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JBMusicPlayer
+//
+//  Created by cnliveJunBo on 2018/9/23.
+//  Copyright © 2018年 cnliveJunBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
